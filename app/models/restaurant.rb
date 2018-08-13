16498a7b98fbe3f18e082d5de519ac6fe932082f
Yixuan Wang <ywang0506@gmail.com>
2018-08-13 11:53:57 -0400

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,36 @@
 class Restaurant
+  @@all =[]
   attr_accessor :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+#  - `Restaurant.find_by_name(name)`
+  #  - given a string of restaurant name, returns the first restaurant that matches
+  def self.find_by_name(name)
+    @@all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  #`Restaurant#customers`
+    #- Returns a **unique** list of all customers who have reviewed a particular restaurant.
+  def my_restarant
+    self_review=Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customer
+    my_restarant.map do |restaurant|
+      restaurant.cutomer
+  end
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,28 @@
+require "pry"
 class Review
-  
-end
 
+  attr_accessor  :content, :rating
+  attr_reader :customer,:restaurant
+  @@all =[]
+  def initialize(customer,restaurant, content, rating)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  #- `Review#customer`
+  #  - returns the customer object for that given review
+  #  - Once a review is created, I should not be able to change the author
+  #self.customer
+
+#  `Review#restaurant`
+  #  - returns the restaurant object for that given review
+  #  - Once a review is created, I should not be able to change the restaurant
+
+
+end
