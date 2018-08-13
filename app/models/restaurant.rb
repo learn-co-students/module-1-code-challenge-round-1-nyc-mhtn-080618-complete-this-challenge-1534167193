7fd195a6644ebeadb8a0c9ea16ae46129cfcfe98
madtab7 <madtab7@gmail.com>
2018-08-13 10:34:36 -0400

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,61 @@
 class Restaurant
-  attr_accessor :name
+  attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
-end
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  def reviews #iterate thru Review.all, return reviews. restaurant == self
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers #iterate thru #reviews, return customers.uniq
+    reviews.map do |review|
+      review.customer.full_name
+    end.uniq
+  end
+
+  def ratings #iterate thru #reviews, return array of all ratings
+    reviews.map do |review|
+      review.rating
+    end
+  end
+
+  def average_star_rating #go thru #ratings array, add totals and find average
+    num_reviews = self.reviews.count
+    total = 0
+    ratings.each do |rating|
+      total += rating
+    end
+    (total / num_reviews)
+  end
+
+  def content_of_review
+    reviews.map do |review|
+      review.content
+    end
+  end
+
+  def longest_review #use #content_of_review, sort by length, return longest
+    content_of_review.sort_by do |content|
+      content.length
+    end.last
+  end
+
+
+end #END CLASS
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,24 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+cust1=Customer.new("Maddy", "Tabing")
+cust2=Customer.new("firstname", "lastname")
+cust3=Customer.new("Christopher", "Robin")
+cust4=Customer.new("Maddy", "lastname")
+
+rest1 = Restaurant.new("Chipotle")
+rest2 = Restaurant.new("Emily")
+rest3 = Restaurant.new("Barbuto")
+rest4 = Restaurant.new("Taco Shop")
+
+review1= Review.new(cust1, rest2, 5, "good")
+review2= Review.new(cust1, rest3, 1, "bad")
+review3= Review.new(cust2, rest3, 4, "ok")
+review4= Review.new(cust3, rest4, 5, "great")
+review5=Review.new(cust3, rest3, 2, "really good")
+review6 = Review.new(cust2, rest3, 1, "just ok")
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
