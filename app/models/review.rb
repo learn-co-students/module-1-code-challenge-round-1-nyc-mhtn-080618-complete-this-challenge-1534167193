377f71815e14ecf43b9c0d521cef8d54dd175b81
kristinatong <kristina.tong@gmail.com>
2018-08-13 10:34:42 -0400

    done with challenge 1

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,34 @@
 class Review
-  
-end
+  @@all = []
 
+  attr_reader :customer, :restaurant
+  attr_accessor :rating, :content
+
+=begin
+  - `Review#customer`
+    - returns the customer object for that given review
+    - Once a review is created, I should not be able to change the author
+  - `Review#restaurant`
+    - returns the restaurant object for that given review
+    - Once a review is created, I should not be able to change the restaurant
+  - `Review#rating`
+    - returns the star rating for a restaurant. This should be an integer from 1-5
+  - `Review#content`
+    - returns the review content, as a string, for a particular review
+=end
+
+  def initialize(customer,restaurant,content,rating)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
+#  - `Review.all`
+#    - returns all of the reviews
+  def self.all
+    @@all
+  end
+
+end
