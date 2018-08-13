c675abe83488cbc505e63b3931be8f3fa9c51aae
github username <github email address>
2018-08-13 10:34:40 -0400

    Submission

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,64 @@
+require 'pry'
+
 class Customer
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :reviews
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @reviews = []
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(nameIn)
+    self.all.find do |customer| # Returns first match of name
+      #binding.pry
+      fullName = customer.first_name + ' ' + customer.last_name
+      fullName == nameIn
+    end
+  end
+
+  def self.find_all_by_first_name(nameIn)
+    self.all.select do |customer|
+      nameIn == customer.first_name
+    end
+  end
+
+  def self.all_names
+    fullList = self.all.select do |customer| # Gets all matching customer objs
+      customer.first_name
+    end
+
+    firstNames = fullList.map do |customer| # Return just first names
+      customer.first_name
+    end
+
+    return firstNames
+  end
+
+  def add_review(restIn, contentIn, ratingIn)
+    myReview = Review.new(self, restIn, ratingIn, contentIn)
+    self.reviews << myReview
+    restIn.reviews << myReview
+  end
+
+  def num_reviews
+    self.reviews.length
+  end
+
+  def restaurants
+    self.reviews.map do |review|
+      review.restaurant
+    end.uniq
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,32 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+customer1 = Customer.new("Justin", "Wang")
+customer2 = Customer.new("Jason", "Wang")
+customer3 = Customer.new("Dan", "Harris")
+customer4 = Customer.new("Justin", "Harris")
+
+restaurant1 = Restaurant.new("Wendy's")
+restaurant2 = Restaurant.new("Something else")
+
+# Customer tests
+test1 = Customer.find_by_name("Justin Wang")
+test2 = Customer.find_all_by_first_name("Justin")
+test3 = Customer.all_names
+test10 = customer1.add_review(restaurant1, "testing", 4)
+test11 = customer1.add_review(restaurant2, "check2222", 3)
+test12 = customer2.add_review(restaurant1, "check322222", 2)
+test13 = customer1.add_review(restaurant1, "check4", 5)
+
+# Restaurant tests
+test4 = Restaurant.find_by_name("Something else")
+
+# Review tests
+test5 = Review.new(customer1, restaurant1, 0, "check")
+test6 = Review.new(customer1, restaurant1, 6, "check")
+test7 = Review.new(customer1, restaurant1, 5, "check")
+test8 = Review.new(customer1, restaurant1, 1, "check")
+test9 = Review.new(customer1, restaurant1, 3, "check")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
